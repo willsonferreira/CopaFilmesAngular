6eb31fe9e6d845def65fde6e058705a80b63544a
Willson Ferreira <willson.ferreira@outlook.com>
2020-06-19 09:18:05 -0300

    Campeonato - Listagem de filmes
    Exibição da lista de filmes

--- src/app/campeonato/campeonato.component.ts
+++ src/app/campeonato/campeonato.component.ts
@@ -1,4 +1,6 @@
+import { CampeonatoService, IFilme } from './campeonato.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-campeonato',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampeonatoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [ 'titulo', 'anoLacamento' ];
+  dataSource: IFilme[];
+  isLoadingResults: boolean;
+  totalFilmesSelecionados: number;
+  filmesSelecionados: IFilme[];
+  filme: IFilme;
+
+  constructor(private campeonatoService: CampeonatoService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoadingResults = true;
+    this.campeonatoService.buscarFilmes()
+    .subscribe(res => {
+      this.dataSource = res;
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
   }
